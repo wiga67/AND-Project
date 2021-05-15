3587653588e049d537b30a4be38a703d982fa365
Baptiste Finck <65229633+wiga67@users.noreply.github.com>
2021-05-15 18:45:43 +0200

    Initial commit

--- app/src/main/java/me/finck/and_project/OfflineModeActivity.java
+++ app/src/main/java/me/finck/and_project/OfflineModeActivity.java
@@ -1,8 +1,13 @@
 package me.finck.and_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class OfflineModeActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class OfflineModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_mode);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        //Return to Main activity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        }
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationDrawer = findViewById(R.id.navigation_drawer);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        toolbar = findViewById(R.id.toolbar);
     }
+
+    }
+
 }
